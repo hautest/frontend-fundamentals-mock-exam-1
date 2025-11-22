a3c81b45f37e6ab9ea49d04f690b8e6389bbe8b5
hautest <dos07008@naver.com>
2025-11-22 20:14:37 +0900

    feat: errorMessage를 받을 수 있는 TextField 개발

---
+++ src/shared/ui/TextField.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps } from 'react';
+import { colors, Flex, TextField as TosslibTextField } from 'tosslib';
+
+const ERROR_MESSAGE_HIGHT = '16px';
+
+interface TextFieldProps extends ComponentProps<typeof TosslibTextField> {
+  errorMessage?: string;
+}
+
+export function TextField({ errorMessage, ...rests }: TextFieldProps) {
+  return (
+    <Flex direction="column">
+      <TosslibTextField {...rests} />
+      {/* 에러메세지가 생길 때 컴포넌트 높이가 바뀌지 않게 처리하기 위해 height를 고정 */}
+      <p
+        css={{
+          fontSize: '12px',
+          lineHeight: ERROR_MESSAGE_HIGHT,
+          visibility: errorMessage ? 'visible' : 'hidden',
+          padding: '0 20px',
+          color: colors.red600,
+          margin: 0,
+          height: ERROR_MESSAGE_HIGHT,
+        }}
+      >
+        {errorMessage}
+      </p>
+    </Flex>
+  );
+}
